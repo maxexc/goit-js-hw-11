01a91f3b41701be9a04591e6fdd8e56dca35a754
Maksym Solomko <max.exc7@gmail.com>
2022-10-22 03:58:34 +0300

    axios

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
 import pictureCard from './templates/cards.hbs';
-import { fetchImg } from './js/fetch_ImgData.js';
+import { axiosImg } from './js/axiosImgData.js';
 import dancingGif from './templates/dancingGif.hbs';
+
+console.log(axiosImg());
 
 const gallery = document.querySelector('.js-gallery-hbs');
 const searchForm = document.querySelector('#search-form');
-const searchBtn = document.querySelector('form');
 
 searchForm.addEventListener('submit', onSearchForm);
 let query;
+let pageNumber = 1;
 
 function onSearchForm(event) {
   event.preventDefault();
   window.scrollTo({ top: 0 });
   // page = 1;
   query = event.currentTarget.searchQuery.value.trim();
-  console.log(query);
+  // console.log(query);
   gallery.innerHTML = '';
 
   if (query === '') {
-    emptySearch();
+    // emptySearch();
     return;
   }
 
-  fetchImg(query)
-    .then(({ data }) => {
+  axiosImg(query, pageNumber)
+    .then(data => {
       console.log(data);
-      console.log(data.totalHits);
-      console.log(data.hits);
+      // console.log(data.totalHits);
+      // console.log(data.hits);
       if (data.totalHits === 0) {
         noImagesFound();
       } else {
@@ -43,4 +45,5 @@ function onSearchForm(event) {
     });
 }
 
-searchBtn.insertAdjacentHTML('beforeend', dancingGif());
+// console.log(query);
+// searchForm.insertAdjacentHTML('beforeend', dancingGif());
---
+++ src/js/axiosImgData.js
@@ -0,0 +1,85 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+export { axiosImg };
+
+Notiflix.Notify.init({
+  // position: 'left-top',
+  cssAnimationStyle: 'zoom',
+  fontSize: '20px',
+  // showOnlyTheLastOne: true,
+});
+
+const API_URL = 'https://pixabay.com/api/';
+const KEY = '30747162-c0f899af5e8792e55f79454a6';
+let totalPage;
+
+async function axiosImg(query, pageNumber) {
+  // let page;
+  return await axios
+    .get(API_URL, {
+      params: {
+        key: KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: pageNumber,
+        per_page: 40,
+      },
+    })
+    .then(async response => {
+      if (response.status !== 200) {
+        return Promise.reject(`Error: ${response.message}`);
+      }
+      if (!response.data.totalHits) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      if (pageNumber === 1 && response.data.totalHits) {
+        Notiflix.Notify.success(
+          `Hooray! We found ${response.data.totalHits} images.`
+        );
+      }
+      if (totalPage <= pageNumber) {
+        Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+      pageNumber += 1;
+      totalPage = Math.ceil(response.data.totalHits / 40);
+      console.log('totalPage:', totalPage);
+      new Error(response.status);
+      return await response.data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+// async function axiosImg(query, pageNumber) {
+//   try {
+//     return await axios.get(API_URL, {
+//       params: {
+//         key: KEY,
+//         q: query,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: 'true',
+//         page: pageNumber,
+//         per_page: 40,
+//       },
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// export { axiosImg };
+
+// async function axiosImg(query, page) {
+//   //   let page = 1;
+//   return await axios.get(
+//     `${API_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+//   );
+// }
