ac8d562b66ecd375c1dbe714e2e7cd7e44fca925
Maksym Solomko <max.exc7@gmail.com>
2022-11-01 03:29:20 +0200

    Update axiosImgData.js

--- src/js/axiosImgData.js
+++ src/js/axiosImgData.js
@@ -13,37 +13,38 @@ const API_URL = 'https://pixabay.com/api/';
 const KEY = '30747162-c0f899af5e8792e55f79454a6';
 
 async function axiosImg(query, pageNumber) {
-  return await axios
-    .get(API_URL, {
-      params: {
-        key: KEY,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-        page: pageNumber,
-        per_page: 40,
-      },
-    })
-    .then(async response => {
-      if (response.status !== 200) {
-        return Promise.reject(`Error: ${response.message}`);
-      }
-      if (!response.data.totalHits) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      if (pageNumber === 1 && response.data.totalHits) {
-        Notiflix.Notify.success(
-          `Hooray! We found ${response.data.totalHits} images.`
-        );
-      }
-      return await response.data;
-    })
-    .catch(error => {
-      console.error(error);
-    });
+  try {
+    return await axios
+      .get(API_URL, {
+        params: {
+          key: KEY,
+          q: query,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: 'true',
+          page: pageNumber,
+          per_page: 40,
+        },
+      })
+      .then(async response => {
+        if (response.status !== 200) {
+          return Promise.reject(`Error: ${response.message}`);
+        }
+        if (!response.data.totalHits) {
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        }
+        if (pageNumber === 1 && response.data.totalHits) {
+          Notiflix.Notify.success(
+            `Hooray! We found ${response.data.totalHits} images.`
+          );
+        }
+        return await response.data;
+      });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // async function axiosImg(query, pageNumber) {
